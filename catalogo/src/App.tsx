149a2be61614be5d7dac61d07a92adba5587972b
Roberto Dos Santos Filho <robertodossantosfilho1001@gmail.com>
2024-10-26 13:07:02 +0000

    initial setup

---
+++ catalogo/src/App.tsx
@@ -0,0 +1,14 @@
+import {ThemeProvider} from "@/components/theme-provider"
+import Home from "./Home"
+
+function App() {
+  return (
+    <>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <Home />
+      </ThemeProvider>
+    </>
+  )
+}
+
+export default App
